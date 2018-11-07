ae8ff84220fe228021bb79946140192ab643e193
Tommy228 <ouazzany.@gmail.com>
2018-11-07 15:25:29 +0100

    Hotfix

--- src/main/java/listeners/ApplicationData.java
+++ src/main/java/listeners/ApplicationData.java
@@ -6,16 +6,22 @@ import javax.persistence.Persistence;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 import javax.servlet.annotation.WebListener;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 @WebListener
 public class ApplicationData implements ServletContextListener {
 
     private static EntityManagerFactory emf;
-    private static final String[] MENUS = {"Dashboard", "Sensors", "Alerts edition"};
+    private static final Map<String, String> MENUS;
+    static
+    {
+        MENUS = new HashMap<>();
+        MENUS.put("Dashboard", "dashboard");
+        MENUS.put("Sensors", "sensors");
+        MENUS.put("Alerts edition", "alerts");
+    }
+
+    private final static String[] MENUS_ORDER = {"Dashboard", "Sensors", "Alerts edition"};
 
     public static final Map<String, String> UNITS;
     static
@@ -39,6 +45,7 @@ public class ApplicationData implements ServletContextListener {
     @Override
     public void contextInitialized(ServletContextEvent event) {
         event.getServletContext().setAttribute("menus", MENUS);
+        event.getServletContext().setAttribute("menusOrder", MENUS_ORDER);
         emf = Persistence.createEntityManagerFactory("client_database");
     }
 
